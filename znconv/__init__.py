f78149986f37a6bc61da7acfae0654d9a89f17be
Fabian <46721498+PythonFZ@users.noreply.github.com>
2021-12-05 23:30:44 +0100

    run black

--- znconv/__init__.py
+++ znconv/__init__.py
@@ -1,8 +1,8 @@
 from .base import ConverterBase
 from .main import ZnEncoder, ZnDecoder
-from .config import register
+from .config import register, config
 from .converter import *
 
-__all__ = ["ConverterBase", "ZnDecoder", "ZnEncoder", "register"]
+__all__ = ["ConverterBase", "ZnDecoder", "ZnEncoder", "register", "config"]
 
 __version__ = "0.0.2"
