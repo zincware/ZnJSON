3e88f0b66a2a73c78df29de6711c00eee97ef5f7
Fabian <46721498+PythonFZ@users.noreply.github.com>
2022-01-11 19:51:15 +0100

    add base64 converter

---
+++ znjson/converter/numpy_converter_base64.py
@@ -0,0 +1,24 @@
+import base64
+import io
+
+import numpy as np
+
+from znjson import ConverterBase
+
+
+class NumpyConverterBase64(ConverterBase):
+    instance = np.ndarray
+    representation = "np.ndarray64"
+    order = 10
+
+    def _encode(self, obj):
+        with io.BytesIO() as f:
+            np.save(f, obj)
+            f.seek(0)
+            return base64.b64encode(f.read()).decode("ascii")
+
+    def _decode(self, value):
+        with io.BytesIO() as f:
+            f.write(base64.b64decode(value))
+            f.seek(0)
+            return np.load(f)
