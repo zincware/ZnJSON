60e2579189d3a9760b938c4dc782650a76563837
Fabian <46721498+PythonFZ@users.noreply.github.com>
2022-01-19 14:12:15 +0100

    bugfix

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,20 +5,12 @@ import pytest
 
 import znjson
 
+znjson.register(znjson.PathlibConverter)
+
 
 @pytest.fixture
 def simple_dict():
     return {"a": 10, "b": 20}
-
-
-@pytest.fixture()
-def pathlib_path():
-    return pathlib.Path("test_path.txt")
-
-
-@pytest.fixture()
-def serialized_pathlib_path():
-    return '{"_type": "pathlib.Path", "value": "test_path.txt"}'
 
 
 def test_encoder_serializable(simple_dict):
@@ -29,3 +21,21 @@ def test_decoder_serializable(simple_dict):
     data_str = json.dumps(simple_dict, cls=znjson.ZnEncoder)
 
     assert simple_dict == json.loads(data_str, cls=znjson.ZnDecoder)
+
+
+def test_decode_pathlib():
+    data_str = '{"_type": "pathlib.Path", "value": "test_path.txt"}'
+
+    assert json.loads(data_str, cls=znjson.ZnDecoder) == pathlib.Path("test_path.txt")
+
+
+def test_decode_pathlib_wo__type():
+    data_str = '{"value": "test_path.txt"}'
+
+    assert json.loads(data_str, cls=znjson.ZnDecoder) == {"value": "test_path.txt"}
+
+
+def test_decode_pathlib_wo_value():
+    data_str = '{"_type": "pathlib.Path"}'
+
+    assert json.loads(data_str, cls=znjson.ZnDecoder) == {"_type": "pathlib.Path"}
