a4691ce34f397f87f311ee724334f699854bf08e
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2022-10-04 14:02:03 +0200

    add old converters for backwards compatibility (#19)

---
+++ znjson/converter/old_converters.py
@@ -0,0 +1,33 @@
+"""Converters that should not be used anymore but are
+ still available for backwards compatibility."""
+import io
+
+import numpy as np
+
+from znjson.converter.numpy_converter_base64 import NumpyConverter
+
+
+class NumpyConverterLatin1(NumpyConverter):
+    """Old numpy converter"""
+
+    representation = "np.ndarray"
+    level = 1
+
+    def encode(self, obj):
+        with io.BytesIO() as file:
+            np.save(file, obj)
+            file.seek(0)
+            return file.read().decode("latin-1")
+
+    def decode(self, value):
+        with io.BytesIO() as file:
+            file.write(value.encode("latin-1"))
+            file.seek(0)
+            return np.load(file)
+
+
+class NumpyConverterBase64(NumpyConverter):
+    """Old numpy converter representation"""
+
+    representation = "np.ndarray64"
+    level = 2
