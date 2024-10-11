9ee98e3e6d3f0f19c175c2ac991bd2eec34f1b16
Fabian Zills <46721498+PythonFZ@users.noreply.github.com>
2024-10-11 19:00:21 +0200

    allow both cls and converter args (#30)
    * allow both cls and converter args
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * format
    
    * bump version test
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/converter/test_numpy_converter.py
+++ tests/converter/test_numpy_converter.py
@@ -20,6 +20,16 @@ def test_encode(numpy_array):
 def test_encode_direct(numpy_array):
     encoded_str = znjson.dumps(numpy_array, converter=znjson.converter.NumpyConverter)
     assert encoded_str.startswith('{"_type": "np.ndarray_b64"')
+    encoded_str = znjson.dumps(
+        numpy_array,
+        cls=znjson.ZnEncoder.from_converters([znjson.converter.NumpyConverter]),
+    )
+    assert encoded_str.startswith('{"_type": "np.ndarray_b64"')
+
+    with pytest.raises(TypeError):
+        _ = znjson.dumps(
+            numpy_array, converter=znjson.converter.NumpyConverter, cls=znjson.ZnEncoder
+        )
 
 
 def test_decode(numpy_array):
@@ -35,6 +45,17 @@ def test_decode_direct(numpy_array):
         numpy_array,
         znjson.loads(encoded_str, converter=znjson.converter.NumpyConverter),
     )
+    np.testing.assert_array_equal(
+        numpy_array,
+        znjson.loads(
+            encoded_str,
+            cls=znjson.ZnDecoder.from_converters([znjson.converter.NumpyConverter]),
+        ),
+    )
+    with pytest.raises(TypeError):
+        _ = znjson.loads(
+            encoded_str, converter=znjson.converter.NumpyConverter, cls=znjson.ZnDecoder
+        )
 
 
 def test_decode_missing_converter(numpy_array):
